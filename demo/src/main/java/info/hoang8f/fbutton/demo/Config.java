47a83425d99a26d9db8d713dc086425deab83c48
Le Van Hoang <hoang8f@gmail.com>
2014-05-10 15:16:35 +0900

    config github url

---
+++ demo/src/main/java/info/hoang8f/fbutton/demo/Config.java
@@ -0,0 +1,10 @@
+package info.hoang8f.fbutton.demo;
+
+/**
+ * Created by hoang8f on 5/10/14.
+ */
+public class Config {
+
+    public static final String GITHUB_URL = "https://github.com/hoang8f/android-flat-button";
+
+}
